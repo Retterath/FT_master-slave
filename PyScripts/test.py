27045ad5c3d77604f13230d667ce9c8786341c54
Retterath <retterathtom@gmail.com>
2021-08-14 21:14:28 +0200

    Created use_keys(), add_keys_linux()
    Issue with Paramiko. Problems with authentications persist since paramiko doesnt accept .ppk files.
    TODO: extend functionality to send the key to remote machine
    Idea: Add funtionality to create a key inside the programm.

--- PyScripts/test.py
+++ PyScripts/test.py
@@ -1,32 +1,19 @@
 from guizero import *
 from tkinter import filedialog
-import paramiko
-hostname = "127.0.0.1"
-port = 22
-user = "tom"
-passwd = "starwars"
-def test1():
-    app = App(title='Lane Control', width=1000, height=600,layout='grid',bg=(88,129,101))
-    app.text_size = 20
-    box_ping = Box(app, width=400, height=200, border=1, grid=[100,500],layout='grid')                          
-    
-    input_box = TextBox(box_ping,grid=[100,400])
-    PushButton(box_ping,text="Ping", grid=[100,300])
-    
-#     button_explore = Button(app,
-#                         text = "Browse Files",
-#                         command = open_window)
-    PushButton(app,command=lambda: open_window(),text="Open explorer",grid=[200,450])
-    app.display()
-def open_window():
-    
-    filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File")
+from pathlib import Path
+import paramiko, time
+
+target_ip = '192.168.1.119'
+target_user = 'tom'
+target_port = 22
+target_password = 161198
+
 def interactiveSSHShell():
     try:
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname, port=port, username=user, password=passwd)
+        client.connect(hostname=target_ip, port=target_port, username=target_user, password=161198)
         while True:
             try:
                 cmd = input("$> ")
@@ -39,33 +26,66 @@ def interactiveSSHShell():
     except Exception as err:
         print(str(err))
 #interactiveSSHShell()
-from pathlib import Path
 
-current_dir = os.path.basename(os.getcwd())
-print(current_dir)
-current_dir = os.path.split(os.getcwd())
-print(current_dir)
-current_dir = os.path.dirname(os.getcwd())
-print(current_dir)
+def select_keys(path):
+    #TODO: open folder and return the file
+    return 1
+def use_key(key): #paramiko cannot parse .ppk files.
+    #try:
+        #TODO: Uses hosts. not keys. Change
+        print("Establishing ssh connection")
+        client = paramiko.SSHClient()
 
+        client.load_system_host_keys()
+        key_file = paramiko.RSAKey().from_private_key_file(key) 
+        '''
+        The key is yet not added in the remote machine. Use add_keys_linux() to continue
+        '''
+        client.connect(hostname=target_ip, port=target_port, username=target_user, 
+                        allow_agent=False,look_for_keys=False, pkey=key_file)
+        
+        client.load_host_keys(filename=key)
+        client.save_host_keys('./Keys/ft_known_hosts')
+        #private_key = paramiko.RSAKey.from_private_key_file(filename=pkey)
+        
+        command = client.exec_command("hostname")
+        print(command.read().decode())
+        print("Connected to server")
+    #except paramiko.AuthenticationException: print("Authentication failed, check password.")
+        
+use_key("C:/Users/rette/.ssh/id_rsa") #private key
 
-curr_path = Path.cwd()
-
-#curr_path = curr_path.parent
-print(curr_path)
-print(list(curr_path.parents))
-
-target_ip = '192.168.1.119'
-target_user = 'tom'
-target_port = 22
-def open_folder():
+def use_keys(): #Add way for paramiko to create the file itself HERE inside the project
     session = paramiko.SSHClient()
     curr_path = Path.cwd()
     curr_path = curr_path.parent
-    session.load_host_keys('C:/Users/rette/source/repos/FT_master-slave/Keys')
+    #session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    session.load_host_keys('./Keys/known_hosts')
+    #session.set_missing_host_key_policy(paramiko.RejectPolicy())
+    #session.set_missing_host_key_policy(paramiko.WarningPolicy())
     session.connect(hostname=target_ip, username=target_user, password="161198")
-    stdin, stdout, stderr = session.exec_command('hostname')
-    print(stdout.read().decode())
+    commands = ['ls /', 'echo $USER', 'hostname', 'sdfgh']
+    for command in commands:
+        print(f"{'#'*10} Executing the command: {command} {'#'*10}")
+        stdin, stdout, stderr = session.exec_command(command)
+        
+        time.sleep(.5)
+        print(stdout.read().decode())
+        err = stderr.read().decode()
+        if err:
+            print(err)
     session.close()
 
-open_folder()
+#RSA: static generate()
+def add_keys_windows(): # %USERPROFILE%\.ssh\id_rsa.pub | ssh tom@192.168.1.119 "cat >> .ssh/authorized_keys"
+    return 1
+def add_keys_linux(): #ssh-copy-id -i ~/.ssh/id_rsa.pub tom@192.168.1.119 -p 22
+    #TODO: extend functionality to send the key to remote machine
+    session = paramiko.SSHClient()
+    session.load_system_host_keys()
+    key_file = paramiko.RSAKey.from_private_key_file("C:/Users/rette/.ssh/id_rsa")
+    session.connect(hostname=target_ip, username=target_user, pkey=key_file,
+                        allow_agent=False, look_for_keys=False)
+
+    session.close()
+
