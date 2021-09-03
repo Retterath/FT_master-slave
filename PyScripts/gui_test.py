b811e831e5d397934bc50506795b2448a9a381a5
Retterath <retterathtom@gmail.com>
2021-09-03 11:43:28 +0200

    updated visualisation on gui

--- PyScripts/gui_test.py
+++ PyScripts/gui_test.py
@@ -1,5 +1,5 @@
 from guizero import *
-from tkinter import ttk #Like the CSS for tkinter
+from tkinter import Grid, ttk #Like the CSS for tkinter
 import tkinter as tk
 
 '''
@@ -15,6 +15,13 @@ Frame	frm	frm_address
 LARGE_FONT = ("Verdana", 12)
 NORM_FONT = ("Verdana", 10)
 SMALL_FONT = ("Verdana", 8)
+border_effects = {
+    "flat": tk.FLAT,
+    "sunken": tk.SUNKEN,
+    "raised": tk.RAISED,
+    "groove": tk.GROOVE,
+    "ridge": tk.RIDGE,
+}
 def popupmsg(msg):
     popup = tk.Tk()
     popup.wm_title("!")
@@ -24,45 +31,43 @@ def popupmsg(msg):
     btn1.pack()
 
 
-class tk_test(tk.Tk):
+class tk_main(tk.Tk):
     def __init__(self, *args, **kwargs): #Runs on initialisation (load stuff)
         #args: pass as much crap as you want (passing through values)
         #kwargs: key-word-arguments (passing through dictionaries)
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.wm_title(self, "FT_Master-Slave")
-        container = tk.Frame(self)
-        container.pack(side='top', fill='both', expand=True)
+        main_frame = tk.Frame(self, background="cyan", relief=tk.RAISED, borderwidth=5)
         
-        container.grid_rowconfigure(0,weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        #Make the window sticky for every case
+        main_frame.grid_columnconfigure(0,weight=1) 
+        main_frame.grid_rowconfigure(1, weight=1)
+        main_frame.grid(row=0, column=0)
 
-        menubar = tk.Menu(container)
+        menubar = tk.Menu(main_frame)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Save settings", command= lambda: popupmsg("Not supported just yet!"))
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=quit)
         menubar.add_cascade(label="File", menu=filemenu)
         tk.Tk.config(self, menu=menubar)
-        #frames are like containers, sections to seperate widgets on the screen
+        #frames are like main_frames, sections to seperate widgets on the screen
         self.frames = {}
 
+        
         for F in (StartPage,PageOne,PageTwo):
-            frame = F(container,self)
+            frame = F(main_frame,self)
             self.frames[F] = frame
-            frame.grid(row=0,column=0, sticky="nsew ")
+            frame.grid(row=3,column=3, sticky="nsew")
+            
         
         self.show_frame(StartPage)
 
-        frame_a = tk.Frame()
-        frame_b = tk.Frame()
 
-        label_a = tk.Label(master=frame_a, text="Im in frame A")
-        label_a.pack()
-        label_b = tk.Label(master=frame_b, text="Im in frame B")
-        label_b.pack()
-
-        frame_a.pack()
-        frame_b.pack()
+        label_a = tk.Label(master=main_frame, text="Im in frame main_frame")
+        label_a.grid(row=2, column=2)
+        label_b = tk.Label(master=main_frame, text="Im in frame main_frame too")
+        label_b.grid(row=2, column=3)
 
 
     def show_frame(self, cont):
@@ -75,7 +80,8 @@ class tk_test(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, master=parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT) 
+        
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT,relief=tk.GROOVE, borderwidth=5) 
         label.pack(pady=10, padx=10) #Padding outside frame
 
         button1 = ttk.Button(self, text="Use password",
@@ -87,19 +93,19 @@ class StartPage(tk.Frame):
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page one", font=LARGE_FONT) 
+        label = ttk.Label(self, text="Page one", font=LARGE_FONT,relief=tk.GROOVE, borderwidth=5) 
         label.pack(pady=10, padx=10) #Padding outside label
 
         button1 = ttk.Button(self, text="Back to Home",
                             command = lambda: controller.show_frame(StartPage))
         button1.pack()
         button2 = ttk.Button(self, text="Page two",
-                            command=lambda: controller.show_frame(PageOne))
-
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page two", font=LARGE_FONT) 
+        label = ttk.Label(self, text="Page two", font=LARGE_FONT,relief=tk.GROOVE, borderwidth=5) 
         label.pack(pady=10, padx=10) #Padding outside label
 
         button1 = ttk.Button(self, text="Back to Home",
@@ -109,6 +115,6 @@ class PageTwo(tk.Frame):
                             command = lambda: controller.show_frame(PageOne))
         button2.pack()
 
-app = tk_test()
-app.geometry("1920x1080")
+app = tk_main()
+app.geometry("1280x720")
 app.mainloop()
---
+++ Resources/tips.md
@@ -0,0 +1,8 @@
+## Python
+_ means the name is to be treated like private
+__ "dunder" means the name is class-specific. Access them from the class itself
+
+## VS CODE
+<p> In your settings.json, set "python.languageServer": "Pylance". 
+This extension works better (in my opinion) than the one
+microsoft uses. https://github.com/microsoft/vscode-python/issues/3977 <p>
